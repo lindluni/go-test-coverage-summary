c0bc74eac5e5d56e9ad416ca8b10e34449ce1d31
Brett Logan <lindluni@github.com>
2022-05-11 11:40:36 -0400

    Update

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const core = require('@actions/core')
+
+async function main() {
+    const file = core.getInput('coverage_file', { required: true, trimWhitespace: true })
+    const rules = core.getInput('coverage_rules', { required: true, trimWhitespace: true })
+    const threshold = core.getInput('coverage_threshold', { required: true, trimWhitespace: true })
+
+    const lines = JSON.parse(fs.readFileSync(file, 'utf8'))
+
+    const results = [
+        [{data: 'Package', header: true}, {data: 'Coverage', header: true}, {data: 'Result', header: true}]
+    ]
+
+    for(const line of lines) {
+        if(line.Output && line.Output.startsWith('coverage')) {
+            const coverage_value = parseFloat(line.Output.split(' ')[1].split('%')[0])
+            if(coverage_value < threshold) {
+                core.setFailed(`Coverage is below ${threshold}% for package: ${line.Package}`)
+                results.push([line.Package, line.Output.split(':')[1].trim(), '❌'])
+            } else {
+                results.push([line.Package, line.Output.split(':')[1].trim(), '✅'])
+            }
+        }
+    }
+
+    await core.summary
+        .addHeading('Code Coverage Report')
+        .addTable(results)
+        .addLink('Code Coverage Guidelines', rules)
+        .write()
+}
+
+main()
